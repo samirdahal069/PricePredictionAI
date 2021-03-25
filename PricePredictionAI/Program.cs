8ca7cb7ce184ac696f7777453cae52fb243834ca
Samir Dahal <samir.dahal@geeks.ltd.uk>
2021-03-25 21:51:55 +0000

    price prediction AI with newyork taxi data in c#

---
+++ PricePredictionAI/Program.cs
@@ -0,0 +1,23 @@
+﻿using PricePredictionAIML.Model;
+using System;
+
+namespace PricePredictionAI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            // Add input data
+            var input = new ModelInput();
+            input.Trip_distance = 2;
+            input.Passenger_count = 1;
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine(result.Score);
+
+        }
+    }
+}
